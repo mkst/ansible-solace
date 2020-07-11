94012d4278934fbeecb375f8a88dfdbd1a794389
rjgu <ricardojosegomezulmke@gmail.com>
2020-07-11 10:05:29 +0100

    doc

--- lib/ansible/modules/network/solace/solace_dmr.py
+++ lib/ansible/modules/network/solace/solace_dmr.py
@@ -1,62 +1,92 @@
-TODO
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------------------------------
+# MIT License
+#
+# Copyright (c) 2020, Solace Corporation, Ricardo Gomez-Ulmke (ricardo.gomez-ulmke@solace.com)
+# Copyright (c) 2020, Solace Corporation, Swen-Helge Huber <swen-helge.huber@solace.com
+# Copyright (c) 2019, Mark Street <mkst@protonmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ---------------------------------------------------------------------------------------------
 
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+import ansible.module_utils.network.solace.solace_utils as su
+from ansible.module_utils.basic import AnsibleModule
 
 DOCUMENTATION = '''
 ---
 module: solace_dmr
 
-short_description: Configure DMR Cluster on Solace Appliances
-
-version_added: "2.9"
+short_description: Configure a DMR cluster objects.
 
 description:
-    - "Allows addition, removal and configuration of DMR Clusters on Solace Brokers in an idempotent manner."
+  - "Allows addition, removal and configuration of DMR cluster objects on Solace Brokers in an idempotent manner."
+  - "Reference: https://docs.solace.com/API-Developer-Online-Ref-Documentation/swagger-ui/config/index.html#/dmrCluster."
 
 options:
-    name:
-        description:
-            - This is the dmrClusterName of the DMR Cluster being configured
-        required: true
-    state:
-        description:
-            - Target state of the DMR Cluster, present/absent
-        required: false
-    host:
-        description:
-            - Hostname of Solace Broker, default is "localhost"
-        required: false
-    port:
-        description:
-            - Management port of Solace Broker, default is 8080
-        required: false
-    secure_connection:
-        description:
-            - If true use https rather than http for querying
-        required: false
-    username:
-        description:
-            - Administrator username for Solace Broker, defaults is "admin"
-        required: false
-    password:
-        description:
-            - Administrator password for Solace Broker, defaults is "admin"
-        required: false
-    settings:
-        description:
-            - JSON dictionary of additional configuration
-        required: false
-    timeout:
-        description:
-            - Connection timeout when making requests, defaults to 1 (second)
-        required: false
-    x_broker:
-        description:
-            - Custom HTTP header with the broker virtual router id, if using a SMEPv2 Proxy/agent infrastructure
-        required: false
+  name:
+    description: Name of the DMR cluster. Maps to 'dmrClusterName' in the API.
+    required: true
+  settings:
+    description: JSON dictionary of additional configuration, see Reference documentation.
+    required: false
+  state:
+    description: Target state. [present|absent].
+    required: false
+    default: present
+  host:
+    description: Hostname of Solace Broker.
+    required: false
+    default: "localhost"
+  port:
+    description: Management port of Solace Broker.
+    required: false
+    default: 8080
+  secure_connection:
+    description: If true, use https rather than http for querying.
+    required: false
+    default: false
+  username:
+    description: Administrator username for Solace Broker.
+    required: false
+    default: "admin"
+  password:
+    description: Administrator password for Solace Broker.
+    required: false
+    default: "admin"
+  timeout:
+    description: Connection timeout in seconds for the http request.
+    required: false
+    default: 1
+  x_broker:
+    description: Custom HTTP header with the broker virtual router id, if using a SMEPv2 Proxy/agent infrastructure.
+    required: false
+
 
 author:
-    - Mark Street (mkst@protonmail.com)
-    - Swen-Helge Huber (swen-helge.huber@solace.com)
+  - Mark Street (mkst@protonmail.com)
+  - Swen-Helge Huber (swen-helge.huber@solace.com)
+  - Ricardo Gomez-Ulmke (ricardo.gomez-ulmke@solace.com)
 '''
 
 EXAMPLES = '''
@@ -79,7 +109,7 @@ EXAMPLES = '''
 
 RETURN = '''
 response:
-    description: The response back from the Solace device
+    description: The response from the Solace Sempv2 request.
     type: dict
 '''
 
